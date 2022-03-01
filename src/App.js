f0711fbf41fbdf0cda496130ca0da5136b69cd96
nikhilstacks <nikhilstacks@gmail.com>
2022-03-01 14:33:58 +0530

    added css modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./test.module.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={styles.test}>test</h1>
+      <h2 className="test">this is a test text</h2>
     </div>
   );
 }
