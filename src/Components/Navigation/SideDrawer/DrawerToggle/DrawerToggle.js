b8ee03d3559099831b87e1319d71eba899adcd3b
nikhilstacks <nikhilstacks@gmail.com>
2022-03-14 20:31:14 +0530

    adding css for toggle button

--- src/Components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/Components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,12 @@
 import React from "react";
+import DrawerToggleCss from "./DrawerToggle.module.css";
 
-const drawerToggle = (props) => <div onClick={props.clicked}>MENU</div>;
+const drawerToggle = (props) => (
+  <div onClick={props.clicked} className={DrawerToggleCss.DrawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default drawerToggle;
