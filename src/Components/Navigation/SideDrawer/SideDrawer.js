94a6b9435d46e929c652a3db7bf20e47bbf11926
nikhilstacks <nikhilstacks@gmail.com>
2022-03-14 20:00:00 +0530

    adding backdrop to menu in sidedrawer

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,23 @@ import React from "react";
 import Logo from "../../logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import SideDrawerCss from "./SideDrawer.module.css";
+import Backdrop from "../../Ui/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
 
 const sideDrawer = () => {
   // ...
   return (
-    <div className={SideDrawerCss.SideDrawer}>
-      <div className={SideDrawerCss.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show />
+      <div className={SideDrawerCss.SideDrawer}>
+        <div className={SideDrawerCss.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
