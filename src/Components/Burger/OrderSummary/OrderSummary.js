3a0576e7510f8cfd2e16abae8848a555f01efd96
nikhilstacks <nikhilstacks@gmail.com>
2022-03-14 20:52:19 +0530

    adding lifeCycle

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Auxiliary";
 import Button from "../../Ui/Button/Button";
 
-const OrderSummary = (props) => {
-  const ingredientsList = Object.keys(props.ingredients).map((iKey) => {
+class OrderSummary extends Component {
+  componentWillUnmount() {
+    console.log("[Order Summary] will update");
+  }
+
+  render() {
+    const ingredientsList = Object.keys(this.props.ingredients).map((iKey) => {
+      return (
+        <li key={iKey}>
+          {iKey}: {this.props.ingredients[iKey]}
+        </li>
+      );
+    });
     return (
-      <li key={iKey}>
-        {iKey}: {props.ingredients[iKey]}
-      </li>
+      <Aux>
+        <h1>Thankyou for Order.</h1>
+        <p>Ingredients of your delicious Burger are:</p>
+        <ul>{ingredientsList}</ul>
+        <p>
+          <strong>Total Price: {this.props.price}$</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancel}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseSuccess}>
+          Continue
+        </Button>
+      </Aux>
     );
-  });
-  return (
-    <Aux>
-      <h1>Thankyou for Order.</h1>
-      <p>Ingredients of your delicious Burger are:</p>
-      <ul>{ingredientsList}</ul>
-      <p>
-        <strong>Total Price: {props.price}$</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancel}>
-        Cancel
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseSuccess}>
-        Continue
-      </Button>
-    </Aux>
-  );
-};
+  }
+}
 
 export default OrderSummary;
--- src/Components/Ui/Modal/Modal.js
+++ src/Components/Ui/Modal/Modal.js
@@ -19,4 +19,4 @@ const Modal = (props) => (
   </Aux>
 );
 
-export default Modal;
+export default React.memo(Modal);
